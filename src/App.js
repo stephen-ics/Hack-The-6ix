e3a362228031c1abd2e602d8311ef16b90671307
stephen-ics <stephenni1234@gmail.com>
2022-08-20 00:15:50 -0400

    responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import React, { Suspense } from "react";
+ 
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import About from './pages/About'
+ 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return (
+   <div className="App">
+     <BrowserRouter>
+       <Suspense fallback={<div>Page Loading...</div>}>
+         <Navbar />
+         <Routes>
+           <Route path="/" exact element={<Home />} />
+           <Route path="/about" exact element={<About />} />
+         </Routes>
+       </Suspense>
+     </BrowserRouter>
+   </div>
+ );
 }
-
+ 
 export default App;
+
